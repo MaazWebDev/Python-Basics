5b7b3c0fe5650863177d4e8d2688f7224063ab39
MaazWebDev <maazkhan66376@gmail.com>
2025-07-12 16:26:33 +0500

    Data Types

---
+++ DataTypes/dataType.py
@@ -0,0 +1,113 @@
+# Data Types In Python ;
+
+#There Two Types Of Data Types In Python
+# https://app.eraser.io/workspace/oKpScDjNGei3NCGzrIsE?origin=share
+
+# 1.Immutable
+# 2.Mutable
+
+# 1.Mutable 
+# List
+# Set
+# Dictonary
+# ByteArray
+# MemoryView
+
+# 2.Immutable
+# String
+# Integar
+# Floating Point Numer
+# Boolean
+# Tuples
+# Frozen Sets
+# Bytes
+# Complex
+# Range
+# NoneType
+
+# List
+
+myList = [1,2,3,["a","b","c",4.5],5,6]
+print(myList)
+print(type(myList))
+
+# Set 
+
+mySet = {"apple", "banana", "cherry"}
+print(mySet)
+print(type(mySet))
+
+# Dictonary
+
+myDict = {"name" : "Maaz" , "age" : 20 }
+print(myDict)
+print(type(myDict))
+
+# ByteArray
+
+myByteArray = bytearray(5)
+print(myByteArray)
+print(type(myByteArray))
+
+# MemoryView
+
+myMemoryView = memoryview(bytes(5))
+print(myMemoryView)
+print(type(myMemoryView))
+
+# String
+
+myString = "BSAI" ;
+print(myString)
+print(type(myString))
+
+# Number
+
+myNumber = 43 ;
+print(myNumber)
+print(type(myNumber))
+
+# Floating Point Number
+
+myFloat = 20.25
+print(myFloat)
+print(type(myFloat))
+
+# Boolean
+
+myBoolean = True
+print(myBoolean)
+print(type(myBoolean))
+
+# Comples
+
+myComplex = 2j
+print(myComplex)
+print(type(myComplex))
+
+# Range
+
+myRange = range(10)
+print(myRange)
+print(type(myRange))
+
+# Bytes
+
+myByte = b"Maaz"
+print(myByte)
+print(type(myByte))
+
+# NoneType
+myNone = None 
+print(myNone)
+print(type(myNone))
+
+# Tuple 
+myTuple = ("Maaz","Ku","BsAi")
+print(myTuple)
+print(type(myTuple))
+
+# Frozen Sets
+myFrozenSet = frozenset({"a","b","c"})
+print(myFrozenSet)
+print(type(myFrozenSet))
